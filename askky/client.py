c34488db0d609d8f406ca59ed41302826a487159
akanksha_enigma <akanksha@buildenima.com>
2018-06-23 18:27:41 +0530

    client setup

--- askky/client.py
+++ askky/client.py
@@ -1,12 +1,12 @@
 import json
 import requests
 
-class callSurvey:
+class Client:
     def __init__(self,privateKey=None):
         self.base_url = "https://api.askky.co/"
         self.privateKey = privateKey
 
-    def survey(self, survey_id, user_id, **kwargs):
+    def trigger_survey(self, survey_id, user_id, **kwargs):
         """"
         trigger specific survey for a user
         Args:
@@ -25,14 +25,5 @@ class callSurvey:
         print ('data',json.dumps(data))
         headers = {'Content-Type': 'application/json'}
         response = requests.post(url,data=str(json.dumps(data)),headers=headers)
-        print ('response',response.json())
 
         return response
-
-
-# def test():
-#     obj = callSurvey(privateKey="3d3209966caf8b781db16005c320a29701776613247bb80118")
-#     response = obj.survey(survey_id="aszx2f8b75de2c6",user_id="abc")
-
-
-# test()
